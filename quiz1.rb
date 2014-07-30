f930a0b3f7766dfef6c01bf1bd0899be02f466e0
emperorliu <mangomystery@gmail.com>
2014-07-30 12:29:12 -0700

    adding quiz

---
+++ quiz1.rb
@@ -0,0 +1,19 @@
+1. a = 1
+
+2. Array is indexed by its order, whereas hashes include key-value pairs
+
+3. arr = [1,2,3,3] returns => [1,2,3,3]
+  [1,2,3,3].uniq returns => [1,2,3]
+  [1,2,3,3].uniq returns => [1,2,3]
+
+4. 'select' uses a boolean and returns an array where that boolean is true. 'map' maps what you want to do to each item in an array. 
+
+  For example, if I wanted to create an array where it only retrieved the odd numbers of my existing array, then I would use select, and not map.
+
+  5. h = {"string" => "blahblah", "string" => "blahblah"}
+
+  6. "no way!"
+
+  7. 4
+
+  8. error, variable scope.
